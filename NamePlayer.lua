aff55d395626093ab8dbdd4f1c51738c4853ef45
Tyrphes <98525287+Tyrphes@users.noreply.github.com>
2022-07-13 11:07:25 +0700

    Create NamePlayer.lua

---
+++ NamePlayer.lua
@@ -0,0 +1,18 @@
+local PlayerNameList = {
+	{151535041,"Arigato","NAVI"},
+	{221605142,"co","Team Liquid"},
+	{295441053,"Thao","NAVI"},
+	{301335488,"Kiet","NAVI"},
+	{781470027,"VoDanh","NAVI"},
+	{1572447078,"3con","NAVI"},
+	{1635709227,"Galaxy","NAVI"},
+	{397984949,"Akuma","Team Liquid"},
+	{145158047,"Dawid","NAVI"},
+	{498435664,"diff","Team Liquid"},
+	{2583235221,"fern","NAVI"},
+	{2202955325,"BinMC","NAVI"},
+	{378191085,"Bap","NAVI"},
+	{2649959498,"Kite","NAVI"},
+	{218402157,"tri","NAVI"}
+}
+return PlayerNameList
